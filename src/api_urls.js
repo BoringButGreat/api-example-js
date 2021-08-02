26d343146a3bff7c3801aa0358c44c84c8aaf220
Minh Reigen <minh@vendorful.com>
2021-08-02 13:46:17 -0700

    separate api version for future easy update

--- src/api_urls.js
+++ src/api_urls.js
@@ -1,3 +1,5 @@
-export const TOKEN_ENDPOINT = "https://api.vendorful.com/auth/v1/token";
-export const VENDORS_API = "https://api.vendorful.com/vendors/v1";
-export const INTAKE_FORMS_API = "https://api.vendorful.com/intake-forms/v1";
+const API_VERSION = "v1";
+
+export const TOKEN_ENDPOINT = `https://api.vendorful.com/auth/${API_VERSION}/token`;
+export const VENDORS_API = `https://api.vendorful.com/vendors/${API_VERSION}`;
+export const INTAKE_FORMS_API = `https://api.vendorful.com/intake-forms/${API_VERSION}`;
